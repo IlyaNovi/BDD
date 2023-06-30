3ba97f8c1ef51abf5a6b39f0ff8d83d84fc5128b
IlyaNov <noil.01@mail.ru>
2023-06-30 23:05:35 +0300

    first commit

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,23 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ValidLoginPage1 {
+    private SelenideElement loginField = $("[data-test-id='login'] input");
+    private SelenideElement passwordField = $("[data-test-id='password'] input");
+    private SelenideElement loginButton = $("[data-test-id='action-login']");
+
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+
+
+}
